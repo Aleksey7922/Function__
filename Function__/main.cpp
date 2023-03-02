cb0e6caf6f17f25013262a0aa55f07c8d18cefeb
HP Pavilion <HP Pavilion@DESKTOP-9CQK3QQ>
2023-03-02 15:42:17 +0300

    Add project files.

---
+++ Function__/main.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+
+int Sum(int a, int b);    //протоип функции,обьявление функции
+int Dif(int a, int b);
+int Product(int a, int b);
+double Quotient(double a, double b);
+int Remivder(int a, int b);
+
+
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int a, b;
+	cout << "ВВЕДИТЕ ДВА ЧИСЛА: "; cin >> a >> b;
+
+	int c = Sum(a, b); //использование функции
+	cout << a << "+" << b << "=" << c << endl;
+	cout << a << "-" << b << "=" << Dif(a, b) << endl;
+	cout << a << "*" << b << "=" << Product(a, b) << endl;
+	cout << a << "/" << b << "=" << Quotient(a, b) << endl;
+	cout << a << "%" << b << "=" << Remivder(a, b) << endl;
+}
+int Sum(int a, int b)   //реализация функции 
+{
+	int c = a + b;
+	return c;
+}
+int Dif(int a, int b)
+{
+	return a - b;
+}
+int Product(int a, int b)
+{
+	return a * b;
+}
+double Quotient(double a, double b)
+{
+	return a / b;
+}
+int Remivder(int a, int b)
+{
+	return a % b;
+}
---
+++ function_HW/main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int Degree(int a, int pow);
+int Factorial(int i, int f);
+
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int c, f = 1, i = 1;
+	cout << "¬ведите число: "; cin >> c;
+	for (; i <= c; i++)f *= i;
+	cout << c << "!" << "=" << Factorial(i, f) << endl;
+
+	int a, n, pow = 1;
+	cout << "¬ведите число: "; cin >> a;
+	cout << "”кажите степень: "; cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		pow *= a;
+	}
+	cout << a << "^" << n << "=" << Degree(a, pow);
+}
+int Factorial(int i, int f)
+{
+	return f;
+}
+int Degree(int a, int pow)
+{
+	return pow;
+}
