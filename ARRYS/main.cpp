74d6ed1eb5883e86910df1f9c0af787c4b3b26d5
HP Pavilion <HP Pavilion@DESKTOP-9CQK3QQ>
2023-03-09 15:19:12 +0300

    The decision is changed

--- ARRYS/main.cpp
+++ ARRYS/main.cpp
@@ -6,29 +6,50 @@ void main()
 {
 	setlocale(LC_ALL, "RUS");
 	const int n = 5;
-	int arr[n] = {};
+	int arr[n] = { };
 	//arr[2];
 	//arr[2] = 123;//обращение к второму элементу массива на запись 
 	//cout << arr[2] << endl;// обращение ко второму злементу на чтение 
 
-	
-	cout << "Введите злементы массива: "; 
-	int sum = 0;
+
+	cout << "Введите злементы массива: ";
+	int sum = 0; double arith_mean = 0;
 	for (int i = 0; i < n; i++)
 	{
 		cin >> arr[i];
-	}
-	for (int i = 0; i < n; i++)
-	{
 		cout << arr[i] << "\t";
-		sum += [i];
-		cout << sum;
 	}
 	cout << endl;
 
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "Сумма элементов массива: " << sum;
+	cout << endl;
 
-
-
+	for (int i = 0; i < 1; i++)
+	{
+		arith_mean = sum / n;
+		cout << "Среднее арифм. элементов: " << arith_mean << endl;
+	}
+	int max = arr[0];
+	int min = arr[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (max < arr[i]) max = arr[i];
+		if (min > arr[i]) min = arr[i];
+	}
+	cout << "Максимальный элемент:  " << max << endl;
+	cout << "Mинимальный элемент:  " << min << endl;
 
 
 }
+
+
+
+
+
+
+
+
--- function_HW/main.cpp
+++ function_HW/main.cpp
@@ -2,31 +2,36 @@
 using namespace std;
 
 int Degree(int a, int pow);
-int Factorial(int i, int f);
+int Factorial(int n);
 
 void main()
 {
 	setlocale(LC_ALL, "RUS");
 
-	int c, f = 1, i = 1;
-	cout << "Введите число: "; cin >> c;
-	for (; i <= c; i++)f *= i;
-	cout << c << "!" << "=" << Factorial(i, f) << endl;
+	int n;
+	cout << "Введите число: "; cin >> n;
+	cout << n << "!=" << Factorial(n) << endl;
 
-	int a, n, pow = 1;
+	int a;
 	cout << "Введите число: "; cin >> a;
 	cout << "Укажите степень: "; cin >> n;
+	
+	cout << a << "^" << n << "=" << Degree(a, n);
+}
+int Factorial(int n)
+{
+	int f = 1;
+	for (int i = 1; i <= n; i++)
+		f *= i;
+	return f;
+}
+int Degree(int a, int n)
+{
+	int pow = 1;
 	for (int i = 1; i <= n; i++)
 	{
 		pow *= a;
 	}
-	cout << a << "^" << n << "=" << Degree(a, pow);
-}
-int Factorial(int i, int f)
-{
-	return f;
-}
-int Degree(int a, int pow)
-{
+
 	return pow;
 }
